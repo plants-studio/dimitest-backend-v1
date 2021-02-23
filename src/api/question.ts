34aee37e55a5932f483e38fdd27113589e59443f
al.co.js <al.co.js@kakao.com>
2021-02-23 10:28:30 +0900

    fix: api routing

--- src/api/question.ts
+++ src/api/question.ts
@@ -7,9 +7,9 @@ import {
 const router = new Router();
 
 router.post('/ch1', ch1);
-router.post('/ch1', ch2);
-router.post('/ch1', ch3);
-router.post('/ch1', typeResult);
+router.post('/ch2', ch2);
+router.post('/ch3', ch3);
+router.post('/result', typeResult);
 router.post('/:key', createQuestion);
 
 export default router;
